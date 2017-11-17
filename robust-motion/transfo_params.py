2fbb57816fb1fe651113501207e2264324596126
Tristan Glatard <tristan.glatard@concordia.ca>
2017-11-17 11:01:56 -0500

    Fixed typos

--- robust-motion/transfo_params.py
+++ robust-motion/transfo_params.py
@@ -3,13 +3,14 @@
 import argparse
 import transfo_utils as tu
 
-parser = ArgumentParser("Prints transformation parameters from xfm file: tx, ty, tz, rx, ry, rz")
+parser = argparse.ArgumentParser("Prints transformation parameters from xfm file: tx, ty, tz, rx, ry, rz")
 parser.add_argument("transfo_file", action="store",
                     help="xfm transformation file.")
-results = parser.parse_args(params)
+results = parser.parse_args()
 
-transfo = tu.read_transfo(results.transfo_file)
-tr_vec = tu.get_tr_vec(transfo)
-rot_mat = get_rot_mat(transfo_mat)
-rot_angles = get_euler_angles(rot_mat)
-print(tr_vec[0], tr_vec[1], tr_vec[2], rot_angles[0], rot_angles[1], rot_angles[2])
+transfo_mat = tu.read_transfo(results.transfo_file)
+tr_vec = tu.get_tr_vec(transfo_mat)
+rot_mat = tu.get_rot_mat(transfo_mat)
+rot_angles = tu.get_euler_angles(rot_mat)
+print("{0}, {1}, {2}, {3}, {4}, {5}".format(tr_vec[0], tr_vec[1], tr_vec[2],
+                                            rot_angles[0], rot_angles[1], rot_angles[2]))
